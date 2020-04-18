759a9289fc279a941650ceb3b41c8e4cf110dd7d
aesher9o1 <aesher9o1@users.noreply.github.com>
2020-04-18 08:56:13 +0000

    deploy: 1618c658d20e84d3cb5168037c05b821e2676cc2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d030c3a2a6eb20d28818545658a07bba.js"
+  "precache-manifest.06639f418b92bf7a4e074b4c9738b69c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

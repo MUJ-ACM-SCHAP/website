9218f1d27c899d6ab08f9ce9d6a5dbfadf8ff7c8
Aashis <Kumar>
2020-04-18 16:17:47 +0530

    Removed base UI

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Client as Styletron } from "styletron-engine-atomic";
-import { Provider as StyletronProvider } from "styletron-react";
-import { LightTheme, BaseProvider } from "baseui";
 import * as serviceWorker from "./serviceWorker";
 
-const engine = new Styletron();
-
 ReactDOM.render(
-  <StyletronProvider value={engine}>
-    <BaseProvider theme={LightTheme}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BaseProvider>
-  </StyletronProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 

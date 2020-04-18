bf2823771d3f69edb15c1aa37ea26fc917f2ef62
Aashis <Kumar>
2020-04-18 12:57:13 +0530

    Firebase initialized and boilerplate removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider } from "baseui";
+import * as serviceWorker from "./serviceWorker";
+
+const engine = new Styletron();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BaseProvider>
+  </StyletronProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
